22b5d8ff6da68eed7d4ed853cfc0d29f9aa68a08
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-04-22 11:56:43 -0700

    submitted

--- src/setOperations.js
+++ src/setOperations.js
@@ -1,16 +1,36 @@
 /**
  * return the union of two sets
  */
-function union(s1, s2) {}
+function union(s1, s2) {
+  return new Set([...s1, ...s2]);
+}
 
 /**
  * return the intersection of two sets
  */
-function intersect(s1, s2) {}
+function intersect(s1, s2) {
+  let intersect = new Set();
+  for (let value of s1) {
+    if (s2.has(value)) {
+      intersect.add(value);
+    }
+  }
+
+  return intersect;
+}
 
 /**
  * return the difference of two sets
  */
-function difference(s1, s2) {}
+function difference(s1, s2) {
+  let difference = new Set();
+  for (let value of s1) {
+    if (!s2.has(value)) {
+      difference.add(value);
+    }
+  }
+
+  return difference;
+}
 
 module.exports = { union, intersect, difference };
